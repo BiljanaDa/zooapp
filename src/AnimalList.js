6fa5c0c039c790a3a300edc48c66bd264b0fdd9e
BiljanaDa <bibidakovic@gmail.com>
2024-01-16 19:00:40 +0100

    add animal

--- src/AnimalList.js
+++ src/AnimalList.js
@@ -9,6 +9,17 @@ function AnimalList() {
     { vrsta: "Ptica", ime: "Raven" },
   ]);
 
+  const [newAnimal, setNewAnimal] = useState([
+    { vrsta: "", ime: "", datumRodjenja: "" },
+  ]);
+
+  const addAnimal = (e) => {
+    e.preventDefault();
+    setAnimals([...animals, newAnimal]);
+
+    setNewAnimal({ vrsta: "", ime: "", datumRodjenja: "" });
+  };
+
   const removeAnimal = (index) => {
     // setAnimals([...animals.slice(0, index), ...animals.slice(index + 1)]);
 
@@ -37,34 +48,64 @@ function AnimalList() {
   };
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Vrsta:</th>
-          <th>Ime:</th>
-          <th>Datum rodjenja:</th>
-        </tr>
-      </thead>
-      <tbody>
-        {animals.map((animal, index) => (
-          <tr key={index}>
-            <th>{animal.vrsta}</th>
-            <th>{animal.ime}</th>
-            <th>
-              {animal.datumRodjenja
-                ? animal.datumRodjenja.toDateString()
-                : "Nepoznat"}
-            </th>
-            <th>
-              <button onClick={() => removeAnimal(index)}>Delete</button>
-            </th>
-            <th>
-              <button onClick={() => moveToTop(index)}>Move To Top</button>
-            </th>
+    <div>
+      <form onSubmit={addAnimal}>
+        <label>Vrsta</label>
+        <input
+          type="text"
+          value={newAnimal.vrsta}
+          onChange={(e) =>
+            setNewAnimal({ ...newAnimal, vrsta: e.target.value })
+          }
+        />
+        <label>Ime</label>
+        <input
+          type="text"
+          value={newAnimal.ime}
+          onChange={(e) => setNewAnimal({ ...newAnimal, ime: e.target.value })}
+        />
+        <label>Datum rodjenja</label>
+        <input
+          type="date"
+          value={newAnimal.datumRodjenja}
+          onChange={(e) =>
+            setNewAnimal({
+              ...newAnimal,
+              datumRodjenja: new Date(e.target.value),
+            })
+          }
+        />
+        <button type="submit">Add Animal</button>
+      </form>
+      <table>
+        <thead>
+          <tr>
+            <th>Vrsta:</th>
+            <th>Ime:</th>
+            <th>Datum rodjenja:</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {animals.map((animal, index) => (
+            <tr key={index}>
+              <th>{animal.vrsta}</th>
+              <th>{animal.ime}</th>
+              <th>
+                {animal.datumRodjenja
+                  ? animal.datumRodjenja.toDateString()
+                  : "Nepoznat"}
+              </th>
+              <th>
+                <button onClick={() => removeAnimal(index)}>Delete</button>
+              </th>
+              <th>
+                <button onClick={() => moveToTop(index)}>Move To Top</button>
+              </th>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
